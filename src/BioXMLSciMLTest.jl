22793ae2729677a31d8d0b8b6599192d31cf3012
Anand <anandj@uchicago.edu>
2021-04-01 20:26:36 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/BioXMLSciMLTest.jl
@@ -0,0 +1,5 @@
+module BioXMLSciMLTest
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BioXMLSciMLTest
+using Test
+
+@testset "BioXMLSciMLTest.jl" begin
+    # Write your tests here.
+end
